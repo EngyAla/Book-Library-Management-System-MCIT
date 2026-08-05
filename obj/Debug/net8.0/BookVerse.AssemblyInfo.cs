d9cb7acbce39a31210aedcf0f7110d438f1407fd
Engy Alaa <engy55428@gmail.com>
2026-08-06 01:24:05 +0300

    first commit

--- obj/Debug/net8.0/BookVerse.AssemblyInfo.cs
+++ obj/Debug/net8.0/BookVerse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookVerse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0375a1f56dd70a5a40d882366743861b0a82f0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BookVerse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookVerse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
